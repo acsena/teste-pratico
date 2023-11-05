57e4b7a1f657fff3de9b5d23e04b4a0d8c9b59e7
bahamonde <bahamondealine@gmail.com>
2023-11-05 08:39:41 -0300

    tentando conectar banco remoto

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql'); //chamando a dependencia do mysql
 
 const conexao = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+    host: '${{RAILWAY_TCP_PROXY_DOMAIN}}',
+    port: 16812,
     user: "root",
-    password: '',
-    database: 'alunos',
+    password: '{{MYSQL_ROOT_PASSWORD}}',
+    database: 'railway',
 });
 
 module.exports = conexao; //exportar a conexao com o banco
